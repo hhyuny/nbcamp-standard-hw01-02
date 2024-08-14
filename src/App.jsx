50c14817ae44bd92480fd1b8a70d19a2cc7c5292
hhyuny <tiamozz77@gmail.com>
2024-08-14 15:43:07 +0900

    props 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,11 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-  const title = "My Website";
-  const year = "2024";
-
   return (
     <section className="intro-section">
-      <Header title={title} />
+      <Header title={"My website"} />
       <Content />
-      <Footer year={year} />
+      <Footer year={"2024"} />
     </section>
   );
 }
